aa4f91e8ba11be8ecbbba940eb550d311ff9d88f
RekziP <rekziplay@gmail.com>
2024-04-14 03:36:10 +0300

    Change run.py

--- final_solution/solution.py
+++ final_solution/solution.py
@@ -1,5 +1,10 @@
 import typing as tp
-
+from natasha import Segmenter, NewsEmbedding, NewsNERTagger, Doc
+from tqdm import tqdm
+import re
+import pandas as pd
+import json
+import pathlib
 
 EntityScoreType = tp.Tuple[int, float]  # (entity_id, entity_score)
 MessageResultType = tp.List[EntityScoreType]  # list of entity scores,
@@ -10,17 +15,36 @@ MessageResultType = tp.List[EntityScoreType]  # list of entity scores,
 def score_texts(
     messages: tp.Iterable[str], *args, **kwargs
 ) -> tp.Iterable[MessageResultType]:
-    """
-    Main function (see tests for more clarifications)
-    Args:
-        messages (tp.Iterable[str]): any iterable of strings
-        (utf-8 encoded text messages)
 
-    Returns:
-        tp.Iterable[tp.Tuple[int, float]]: for any messages returns
-        MessageResultType object
-    -------
-    Clarifications:
-    >>> assert all([len(m) < 10 ** 11 for m in messages]) # all messages are shorter than 2048 characters
-    """
-    raise NotImplementedError
+    return ner_solution(messages)
+
+
+def ner_solution(messages: tp.Iterable[str]):
+
+    def clean_company_name(name):
+        name = re.sub(r'\b(ПАО|ОАО|ООО|ЗАО)\b', '', name)
+        return name.strip().lower()
+
+    with open(pathlib.Path("data") / 'issuers.json', "r", encoding="utf-8") as f:
+        issuers = json.load(f)
+    segmenter = Segmenter()
+    emb = NewsEmbedding()
+    ner_tagger = NewsNERTagger(emb)
+    results = {}
+
+    for index, row in enumerate(messages):
+
+        doc = Doc(row)
+        doc.segment(segmenter)
+        doc.tag_ner(ner_tagger)
+
+        orgs = [clean_company_name(span.text) for span in doc.spans if span.type in ['ORG', 'LOC', "PER"]]
+
+        results[index] = {}
+        for org in orgs:
+            if org in issuers:
+                results[index][int(issuers[org])] = 0
+    answer = []
+    for item in results.values():
+        answer.append(list(item.items()))
+    return answer
--- test.py
+++ test.py
@@ -1,30 +1,30 @@
-# import json
-# import pandas as pd
-# import numpy as np
-#
-# path = 'data/merged_dataframes.xlsx'
-# text_col_name = 'MessageID'
-# company_col_name = "MergedColumn"
-# score_col_name = "SentimentScore"
-# index_col_name = "ID"
-# df = pd.read_excel(path)
-# df[index_col_name] = np.arange(1, len(df)+1)
-# skip_id = []
-# json_data = {}
-# for i_row in range(len(df)):
-#     row = df.iloc[i_row]
-#     if row[index_col_name] in skip_id:
-#         continue
-#     same_rows = df[row[text_col_name] == df[text_col_name]]
-#     json_data[int(row[text_col_name])] = []
-#     print(f'{i_row} from {len(df)}')
-#     for i in range(len(same_rows)):
-#
-#         json_data[int(row[text_col_name])].append({(same_rows.iloc[i][company_col_name]): int(same_rows.iloc[i][score_col_name])})
-#         skip_id.append(same_rows.iloc[i][index_col_name])
-#
-# with open('data/data.json', 'w') as file:
-#     json.dump(json_data, file)
+import json
+import pandas as pd
+import numpy as np
+
+path = 'data/merged_dataframes.xlsx'
+text_col_name = 'MessageID'
+company_col_name = "MergedColumn"
+score_col_name = "SentimentScore"
+index_col_name = "ID"
+df = pd.read_excel(path)
+df[index_col_name] = np.arange(1, len(df)+1)
+skip_id = []
+json_data = {}
+for i_row in range(len(df)):
+    row = df.iloc[i_row]
+    if row[index_col_name] in skip_id:
+        continue
+    same_rows = df[row[text_col_name] == df[text_col_name]]
+    json_data[int(row[text_col_name])] = []
+    print(f'{i_row} from {len(df)}')
+    for i in range(len(same_rows)):
+
+        json_data[int(row[text_col_name])].append({(same_rows.iloc[i][company_col_name]): int(same_rows.iloc[i][score_col_name])})
+        skip_id.append(same_rows.iloc[i][index_col_name])
+
+with open('data/data.json', 'w') as file:
+    json.dump(json_data, file)
 
 import json
 
